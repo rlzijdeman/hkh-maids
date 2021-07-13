2746c5c438e1a2dfcd2f277da507c35114b3f426
rlzijdeman <richard.zijdeman@iisg.nl>
2021-07-13 12:03:21 +0200

    finished burgerLinker standardization

--- etl/db1888_1909_xlsx_csv_clean.py
+++ etl/db1888_1909_xlsx_csv_clean.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np 
+from fold_to_ascii import fold # for handling diacritics and other special char stuff
+import unidecode
 
 # read in excel file
 df = pd.read_excel("../data/source/NAH inv 940 dienstboderegister 1888-1909.xlsx", dtype='str', na_values='---')
@@ -14,6 +16,13 @@ df.columns = ['regel', 'datumInschrijving', 'familienaam', 'voornaam', 'geboorte
 
 # now go through burgerLinker data cleaning steps
 # https://github.com/CLARIAH/burgerLinker/wiki/01.-Data-standardization
+
+##############################################
+#                                            #
+# burgerLinker cleaning dates and formatting #
+#                                            #
+##############################################
+
 
 # 1 date variables (put into YYYY-MM-DD format)
 df['geboorteDatumCl'] = df['geboorteDatum'].astype(str)
@@ -88,6 +97,79 @@ df['geboorteDatumCl'] = df['geboorteDatumCl'][df['geboorteDatumCl'].str.contains
 # now final step for dates: invert DD-MM-YYYY to YYYY-MM-DD (originally resulted in error on this date: 36-07-1863, fixed above)
 df['geboorteDatumCl'] = pd.to_datetime(df['geboorteDatumCl'], format='%d-%m-%Y').dt.strftime('%Y-%m-%d')
 
+
+#############################################
+#                                           #
+# burgerLinker familynames without prefixes #
+#                                           #
+#############################################
+
+df['familienaamCl'] = df['familienaam'].str.split(',').str[0]
+df['familienaamCl'] = df['familienaamCl'].str.replace(r'(^Vernes/Venes.*$)', 'Vernes')
+df['familienaamCl'] = df['familienaamCl'].str.replace(r'(^H. Vaillard.*$)', 'Vaillard')
+df['familienaamCl'] = df['familienaamCl'].str.replace(r'(^Schöttelndreier.*$)', 'Schottelndreier')
+df['familienaamCl'] = df['familienaamCl'].str.replace(r'(^Mots\. De.*$)', 'Mots')
+df['familienaamCl'] = df['familienaamCl'].str.replace(r'(.\(.*$)', '')
+df['familienaamCl'] = df['familienaamCl'].str.replace(r'(.\[.*$)', '')
+df['familienaamCl'] = df['familienaamCl'].str.replace(r'( \- )', '')
+
+df['familienaamCl'] = df['familienaamCl'].str.replace(r'(^Op de bladen.*$)', 'xxx')
+df['familienaamCl'] = df['familienaamCl'].str.replace(r'(^Op blad.*$)', 'xxx')
+df['familienaamCl'] = df['familienaamCl'].str.replace(r'(^Op dit blad.*$)', 'xxx')
+df['familienaamCl'] = df['familienaamCl'].str.replace(r'(^Eerste gedeelte.*$)', 'xxx')
+df['familienaamCl'] = df['familienaamCl'].str.replace(r'(^Niets vermeld.*$)', 'xxx')
+df['familienaamCl'] = df['familienaamCl'].str.replace(r'(^naam op de laatste regel is.*$)', 'xxx')
+df['familienaamCl'] = df['familienaamCl'].str.replace(r'(^Blad 89 ontbreekt.*$)', 'xxx')
+df['familienaamCl'] = df['familienaamCl'].str.replace(r'(^N\.N\..*$)', 'xxx')
+df['familienaamCl'] = df['familienaamCl'].str.replace(r'(^Leeg.*$)', 'xxx')
+
+df['familienaamCl'] = df['familienaamCl'].replace('xxx', np.nan) # somewhat ugly, but wasn't able to set to nan right away
+
+#####################################################
+#                                                   #
+# burgerLinker replace specific characters in names #
+#                                                   #
+# 'ch' to 'g'                                       #
+# 'c'  to 'k'                                       #
+# 'z'  to 's'                                       #
+# 'ph' to 'f'                                       #
+# 'ij' to 'y'                                       #
+#####################################################
+
+
+df['familienaamCl'] = df['familienaamCl'].astype(str)
+# bug in pandas -> has created 'nan'-string of NaN (missing values) fixing later on
+
+df['familienaamCl'] = (df['familienaamCl'].str.lower()
+                                    .str.strip() 
+                                    .map(lambda x: unidecode.unidecode(x)) )
+df['familienaamCl'] = df['familienaamCl'].str.replace(r' ', '')
+df['familienaamCl'] = df['familienaamCl'].str.replace(r'\.', '')
+df['familienaamCl'] = df['familienaamCl'].str.replace(r'(?i)ch', 'g') #(?i) ignores case
+df['familienaamCl'] = df['familienaamCl'].str.replace(r'([Cc])', 'k') # either capital C or regular c
+df['familienaamCl'] = df['familienaamCl'].str.replace(r'([Zz])', 's')
+df['familienaamCl'] = df['familienaamCl'].str.replace(r'(?i)ph', 'f')
+df['familienaamCl'] = df['familienaamCl'].str.replace(r'(?i)ij', 'y')
+df['familienaamCl'] = df['familienaamCl'].replace('nan', '') # should be fixed in pandas 2.4
+
+df['voornaamCl'] = df['voornaam'].astype(str)
+df['voornaamCl'] = (df['voornaamCl'].str.lower()
+                                    .str.strip() 
+                                    .map(lambda x: unidecode.unidecode(x)) )
+df['voornaamCl'] = df['voornaamCl'].str.replace(r' ', '')
+df['voornaamCl'] = df['voornaamCl'].str.replace(r'\.', '')
+df['voornaamCl'] = df['voornaamCl'].str.replace(r'(?i)ch', 'g')
+df['voornaamCl'] = df['voornaamCl'].str.replace(r'([Cc])', 'k')
+df['voornaamCl'] = df['voornaamCl'].str.replace(r'([Zz])', 's')
+df['voornaamCl'] = df['voornaamCl'].str.replace(r'(?i)ph', 'f')
+df['voornaamCl'] = df['voornaamCl'].str.replace(r'(?i)ij', 'y')
+df['voornaamCl'] = df['voornaamCl'].str.replace(r'(?i)nietsvermeld', '')
+df['voornaamCl'] = df['voornaamCl'].replace('nan', '') # should be fixed in pandas 2.4
+
+
+
+
+# df['sport'] = df.sport.str.replace(r'(^.*ball.*$)', 'ball sport')
 
 # write out as .csv file
 df.to_csv("../data/derived/db1888_1909_clean.csv")
